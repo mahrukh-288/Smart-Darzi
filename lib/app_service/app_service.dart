2cf4b4f2c2cf3e7aa9dcd66d10128c2cfc680a2f
Mahrukh Zafar <mahrukhzafar28@gmail.com>
2024-01-20 14:18:40 +0500

    base url updated

--- lib/app_service/app_service.dart
+++ lib/app_service/app_service.dart
@@ -7,9 +7,9 @@ import '../models/order.dart';
 class AppService {
   final dio = Dio();
 
-  static const customerBaseUrl = 'http://tech.shahbaznawaz.com/v1/user';
-  static const orderBaseUrl = 'http://tech.shahbaznawaz.com/v1/order';
-  static const adminBaseUrl = 'http://tech.shahbaznawaz.com/v1/admin';
+  static const customerBaseUrl = 'https://tech.shahbaznawaz.com/v1/user';
+  static const orderBaseUrl = 'https://tech.shahbaznawaz.com/v1/order';
+  static const adminBaseUrl = 'https://tech.shahbaznawaz.com/v1/admin';
   static const String getAllCustomersUrl = '/allCustomers';
 
   Future<Response> registerCustomer(Customer customer) async {
